0d8642e238fc00eef4fade7cf0db9b8bac957a48
Phillip_Chan <phillip.chan@liferay.COM>
2016-09-22 12:28:46 -0700

    Refactored Search Module to be more MVC

--- src/app/modules/search/search.controller.js
+++ src/app/modules/search/search.controller.js
@@ -1,30 +1,8 @@
-search.controller("searchController", function($scope, searchData, $http, stringUtils, hotkeys) {
+search.controller("searchController", function($scope, searchModel, searchUtils, stringUtils, hotkeys) {
 	$scope.searchTerm = "";
-	$scope.store = {};
 	$scope.searchResults = [];
-
-	var getTitle = function(string) {
-		var beginning = stringUtils.nth_occurrence(string, '">', 1);
-		var end = string.indexOf('</h1>');
-
-		return string.slice(beginning + 2, end);
-	};
-
-	var getPage = function(string) {
-		var beginning = stringUtils.nth_occurrence(string, '"', 1);
-		var end = stringUtils.nth_occurrence(string, '"', 2);
-		var page = string.slice(beginning + 1, end);
-
-		return page;
-	};
-
-	var getBody = function(string) {
-		var lookingFor = '</h1>';
-		var beginning = stringUtils.nth_occurrence(string, lookingFor, 1);
-		var body = string.slice(beginning + lookingFor.length, string.length);
-
-		return stringUtils.stripHTML(body);
-	};
+	$scope.searchIndex = searchModel.searchIndex;
+	$scope.store = searchModel.store;
 
 	$scope.toggleSearch = function(mode) {
         var $searchContainer = $('.search-container');
@@ -43,31 +21,8 @@ search.controller("searchController", function($scope, searchData, $http, string
         }
     };
 
-	// initialize + build search index
-	$scope.searchIndex = lunr(function() {
-		this.ref('page');
-		this.field('title');
-		this.field('body');
-	});
-
-	hotkeys.add({
-        combo: '/',
-        callback: function() {
-            $scope.toggleSearch('show');
-        }
-    });
-
-    hotkeys
-    	.bindTo($scope)
-	    .add({
-	        combo: 'esc',
-
-	        callback: function() {
-	            $scope.toggleSearch('hide');
-	        }
-	    });
-
-	$scope.searchInputHandler = function(term, event) {
+    $scope.searchInputHandler = function(term, event) {
+    	console.log($scope.searchIndex)
 		if (event.key == "Escape") {
 			$scope.toggleSearch('hide');
 		} 
@@ -85,36 +40,20 @@ search.controller("searchController", function($scope, searchData, $http, string
 		});		
 	};
 
-	$http(
-		{
-        	method: 'GET',
-	    	url: 'src/app/modules/search/searchindex.html'
-		})
-		.then(function successCallback(response) {
+	hotkeys.add({
+        combo: '/',
+        callback: function() {
+            $scope.toggleSearch('show');
+        }
+    });
 
-		    let resultsArray = response.data.split('h1 id=');
+    hotkeys
+    	.bindTo($scope)
+	    .add({
+	        combo: 'esc',
 
-			for (var x = 0; x < resultsArray.length; x++) {
-				// decode HTML
-				var decoded = stringUtils.decodeHTML(resultsArray[x]);
-				var currentTitle = getTitle(decoded);
-				var currentPage = getPage(decoded);
-				var currentBody = stringUtils.cutString(getBody(decoded), 300);
-				currentBody += '...';
-
-				// add each article to search index
-				$scope.searchIndex.add({
-					body: currentBody.slice(0, 5),
-					page: currentPage,
-					title: currentTitle
-				});
-
-				// add it to our data store
-				$scope.store[currentPage] = {
-					body: currentBody,
-					title: currentTitle,
-					urlTitle: currentPage
-				};
-			}
-		});
+	        callback: function() {
+	            $scope.toggleSearch('hide');
+	        }
+	    });
 });
